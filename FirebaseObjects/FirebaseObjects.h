cf8234961f977b7d1a7ebcd5424d7180a9230a64
Sean Clark Hess <seanhess@gmail.com>
2013-05-24 14:48:58 -0600

    Initial Commit

---
+++ FirebaseObjects/FirebaseObjects.h
@@ -0,0 +1,13 @@
+//
+//  FirebaseObjects.h
+//  FirebaseObjects
+//
+//  Created by Sean Hess on 5/24/13.
+//  Copyright (c) 2013 Sean Hess. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FirebaseObjects : NSObject
+
+@end
---
+++ FirebaseObjects/FirebaseObjects.m
@@ -0,0 +1,13 @@
+//
+//  FirebaseObjects.m
+//  FirebaseObjects
+//
+//  Created by Sean Hess on 5/24/13.
+//  Copyright (c) 2013 Sean Hess. All rights reserved.
+//
+
+#import "FirebaseObjects.h"
+
+@implementation FirebaseObjects
+
+@end
